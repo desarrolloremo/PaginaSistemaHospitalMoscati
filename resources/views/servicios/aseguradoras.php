544978f38a58c94d3720f4033302cc134102badc
desarrolloremo <gustavo.lopez@remo.mx>
2020-04-23 18:29:50 -0500

    23/ABRIL/2020

--- resources/views/servicios/aseguradoras.php
+++ resources/views/servicios/aseguradoras.php
@@ -14,9 +14,9 @@
 
 
     <div class="section-header">
-        <h2> <i class="fas fa-caret-right"></i> Convenios con aseguradoras</h2>
+        <h2> <i class="fas fa-caret-right"></i> PRÓXIMAMENTE</h2>
     </div>
-
+    <!--
     <p class="text-justify">Hospital San José Moscati presta servicios sanitarios a través de diferentes acuerdos, a pacientes asegurados en distintas
         compañías de salud, bien sean seguros de cuadro médico o de reembolso de gastos. Además de estos, contamos con numerosos
         acuerdos particulares con empresas y organizaciones de distinta naturaleza, empresas autoaseguradoras de accidente
@@ -32,19 +32,19 @@
     background-position: 50% 50%;
     background-size: contain;
     background-repeat: no-repeat;">
-            <!--<div class="margen-lateral " style="top: 110%;">
+            <div class="margen-lateral " style="top: 110%;">
                 <div class="more-info">
                     <a href="./img/SitioPrincipal/promociones/more.png" target="_Blank">
                     <span class="txt">Visite su web</span>
                     <img src="../img/SitioPrincipal/servicios/more.png" alt=""></a>
                 </div>
-            </div>-->
+            </div>
         </div>
         
     </div>
     
     <br>
     <br>
-    <br>
+    <br>-->
 </div>
 @endsection
