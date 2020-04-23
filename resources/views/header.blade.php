7d957be605e9cc50dd8302618f60424b19265967
desarrolloremo <gustavo.lopez@remo.mx>
2020-04-23 17:20:29 -0500

    23/ABRIL/2020

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -39,7 +39,7 @@
             <i class="fas fa-phone" style="margin-left: 20%;"></i> URGENCIAS: +52 442 388 5420
           </div>
         </div>
-        <div class="col-lg 8" style="padding-right: 0px; padding-left: 0px;">
+        <div class="col-lg-8" style="padding-right: 0px; padding-left: 0px;">
           <div class="emergencias">
             <a href="https://www.instagram.com/moscatiqro/" target="_blank"><i class="fab fa-instagram" aria-hidden="true"></i></a>
             <a href="https://www.facebook.com/Moscatiqro/" target="_blank"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
@@ -50,7 +50,7 @@
             <a href="{{route('/')}}"><img src="/img/SitioPrincipal/logo.png" alt="" style="width:250px"></a>
           </div>
         </div>
-        <div class="col-lg 8" style="padding-right: 0px; padding-left: 0px;">
+        <div class="col-lg-8" style="padding-right: 0px; padding-left: 0px;">
         <!-- Main Navigation -->
         <nav class="main_nav">
           <ul class="d-flex flex-row align-items-center justify-content-start">
